474aaaf5da4f8dc8a9c2527d153fff9e513c5c24
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-01 18:32:16 -0500

    background transitions smoothed out

--- states/js/load.js
+++ states/js/load.js
@@ -2,7 +2,7 @@ var loadState = {
   preload: function() {
     var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#fff'});
 
-    game.load.image('sky', 'assets/background.png');
+    game.load.image('sky', 'assets/gameBackground.jpg');
     game.load.image('ufo', 'assets/ufo.png')
     game.load.image('ground', 'assets/ground.png');
     game.load.image('platform', 'assets/happycloudbig.png');
