83bd9185590c54812394239f5f9a650199b17489
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-05 15:26:49 -0500

    added win sound

--- states/js/load.js
+++ states/js/load.js
@@ -3,11 +3,12 @@ var loadState = {
     var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#fff'});
 
     game.load.image('sky', 'assets/gameBackground.jpg');
-    game.load.image('ufo', 'assets/ufo.png')
+    game.load.image('ufo', 'assets/ufo.png');
     game.load.image('ground', 'assets/ground.png');
     game.load.image('platform', 'assets/happycloudbig.png');
     game.load.image('win', 'assets/wdud2.png');
-    game.load.image('menu', 'assets/menuscreen.png')
+    game.load.image('menu', 'assets/menuscreen.png');
+    game.load.audio('winSound', 'assets/win.wav');
     game.load.audio('jump', 'assets/jump.wav');
     game.load.audio('music', 'assets/music.wav');
     game.load.spritesheet('dude', 'assets/yellow.png', 79, 95);
--- states/js/win.js
+++ states/js/win.js
@@ -1,5 +1,7 @@
 var winState = {
   create: function() {
+    winSound = game.add.audio('winSound')
+    winSound.play();
     var winLabel = game.add.image(0, 0, 'win');
     var startLabel = game.add.text(10, 10, 'Press W to restart!', {font: '25px Courier', fill: '#000'});
     var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
