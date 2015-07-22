d4ae9d5181f6a6f1cedeb4cbb504bd16420e3f1c
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-22 15:15:42 -0500

    converted audio to ogg format

--- js/load.js
+++ js/load.js
@@ -2,16 +2,16 @@ var loadState = {
   preload: function() {
     var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#fff'});
 
+    game.load.spritesheet('dude', 'assets/yellow.png', 79, 95);
     game.load.image('sky', 'assets/gameBackground.jpg');
     game.load.image('ufo', 'assets/ufo.png');
     game.load.image('ground', 'assets/ground.png');
     game.load.image('platform', 'assets/happycloudbig.png');
     game.load.image('win', 'assets/wdud2.png');
     game.load.image('menu', 'assets/menuscreen.png');
-    game.load.audio('winSound', 'assets/win.wav');
-    game.load.audio('jump', 'assets/jump.wav');
+    game.load.audio('winSound', 'assets/win.ogg');
+    game.load.audio('jump', 'assets/jump.ogg');
     game.load.audio('music', 'assets/music.ogg');
-    game.load.spritesheet('dude', 'assets/yellow.png', 79, 95);
   },
   create: function() {
     game.state.start('menu');
