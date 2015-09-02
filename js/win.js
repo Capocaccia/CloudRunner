bc5f9fa67b26314427ebce266543b6844c0e12d3
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-09-02 17:18:25 -0500

    fixed error in restart

--- js/win.js
+++ js/win.js
@@ -4,8 +4,8 @@ var winState = {
     winSound.play();
     var winLabel = game.add.image(0, 0, 'win');
     var startLabel = game.add.text(10, 10, 'Press ⬇ to restart!', {font: '25px Courier', fill: '#000'});
-    var up = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    up.onDown.addOnce(this.restart, this);
+    var down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    down.onDown.addOnce(this.restart, this);
   },
   restart: function() {
     game.state.start('menu');
