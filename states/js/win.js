2229825cd5cd026aed4391b7aacaa7913d916b78
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-09-02 17:24:41 -0500

    changed restart controls

--- states/js/win.js
+++ states/js/win.js
@@ -3,9 +3,9 @@ var winState = {
     winSound = game.add.audio('winSound')
     winSound.play();
     var winLabel = game.add.image(0, 0, 'win');
-    var startLabel = game.add.text(10, 10, 'Press W to restart!', {font: '25px Courier', fill: '#000'});
-    var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    wkey.onDown.addOnce(this.restart, this);
+    var startLabel = game.add.text(10, 10, 'Press ⬇ to restart!', {font: '25px Courier', fill: '#000'});
+    var down = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    down.onDown.addOnce(this.restart, this);
   },
   restart: function() {
     game.state.start('menu');
